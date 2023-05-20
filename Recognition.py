b007dda9525dbd74228d6e611e2916a4df3807ac
Tran Dai Phu <113273365+PhuTd03@users.noreply.github.com>
2023-05-21 04:24:13 +0700

    Add files via upload

---
+++ Recognition.py
@@ -0,0 +1,113 @@
+import csv
+import cv2
+import numpy as np
+import pandas as pd
+import os
+import pickle
+from pre_process import preprocess_img
+
+# ip = "rtsp://admin:ZSXNWK@192.168.225.155:554/H.264"
+
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FPS, 10)  # set frame rate to 10 fps
+
+# Define the cropping coordinates (left, top, right, bottom)
+crop_coords = (1080//2 - 360, 1920//2 - 480, 1080//2 + 360, 1920//2 + 480)
+
+# load the trained model from file
+with open('SVM.pkl', 'rb') as f:
+    svm = pickle.load(f)
+
+with open('pca.pkl', 'rb') as f:
+    pca = pickle.load(f)
+
+# Define the path to the saved face images and labels
+data_dir = 'face_data'
+
+# Create a new CSV file for attendance
+attendance_file = open('attendance.csv', mode='w', newline='')
+attendance_writer = csv.writer(attendance_file)
+
+# Write headers to the CSV file
+attendance_writer.writerow(['Name', 'ID', 'Attendance'])
+
+# Load the images and labels from the saved files
+for label in os.listdir(data_dir):
+    name, id = label.split('_')
+    attendance_writer.writerow([name, id, 'Absent'])
+
+# Close the CSV file
+attendance_file.close()
+
+# Load CSV file into dataframe
+df = pd.read_csv('attendance.csv')
+
+# Load the Haar Cascade classifier for face detection
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+# Start the webcam
+# cap = cv2.VideoCapture(0)
+
+# Set the threshold for recognizing unknown faces
+threshold = 0.8
+
+while True:
+    # Read a frame from the webcam
+    ret, frame = cap.read()
+
+    # Crop frame into image size
+    frame = frame[crop_coords[0]: crop_coords[2], crop_coords[1]: crop_coords[3]]
+
+    if not ret:
+        break
+
+    # Convert the frame to grayscale for face detection
+    gray = preprocess_img(frame)
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    # Loop through each face and recognize it
+    for (x, y, w, h) in faces:
+        # Extract the face region from the frame and resize it to the same size as the training data
+        face_img = cv2.resize(gray[y:y+h, x:x+w], (64, 64))
+
+        # Flatten the face image to a 1D array
+        face_img_flat = face_img.reshape(1, -1)
+
+        # Transform the face image using the same PCA object used for training
+        face_img_pca = pca.transform(face_img_flat)
+
+        # Predict the label of the face using the SVM classifier
+        confidence_scores = svm.predict_proba(face_img_pca)
+        print(confidence_scores)
+        label = svm.predict(face_img_pca)
+
+        if np.max(confidence_scores) >= threshold:
+            # Draw a rectangle around the face and label it with the predicted name
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, str(label[0]), (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+            name, id = label[0].split('_')
+            # Find row corresponding to student ID
+            row_index = df.loc[df['ID'] == id].index[0]
+
+            # Update attendance for that row
+            df.at[row_index, 'Attendance'] = 'Present'
+        else:
+            # Draw a rectangle around the face and label it as "unknown"
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(frame, 'Unknown', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+    # Display the frame with the recognized faces
+    cv2.imshow('Face Recognition', frame)
+
+    # Exit the program if the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+        # Save updated dataframe back to CSV file
+        df.to_csv('attendance.csv', index=False)
+        break
+
+# Release the webcam and close all windows
+cap.release()
+cv2.destroyAllWindows()
---
+++ Train.py
@@ -0,0 +1,81 @@
+import os
+import pickle
+import cv2
+import numpy as np
+from scipy.stats import loguniform
+from sklearn.calibration import CalibratedClassifierCV
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC, LinearSVC
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+
+# Define the path to the saved face images and labels
+data_dir = 'face_data'
+
+# Load the images and labels from the saved files
+face_images = []
+face_labels = []
+for label in os.listdir(data_dir):
+    label_dir = os.path.join(data_dir, label)
+    if os.path.isdir(label_dir):
+        for image_file in os.listdir(label_dir):
+            image_path = os.path.join(label_dir, image_file)
+            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            face_images.append(image)
+            face_labels.append(label)
+
+# Convert the image and label lists to numpy arrays
+face_images = np.array(face_images)
+face_labels = np.array(face_labels)
+
+# Flatten the array
+face_images = face_images.reshape(face_images.shape[0], -1)
+
+# Split the data into training and testing sets
+X = face_images
+y = face_labels
+
+# Shuffle X and y
+indices = np.arange(len(X))
+np.random.shuffle(indices)
+
+X = X[indices]
+y = y[indices]
+
+# Perform PCA to reduce the dimensionality of the face embeddings
+n_components = 50  # Number of principal components to keep
+pca = PCA(n_components=n_components, svd_solver="randomized", whiten=True)
+X_pca = pca.fit_transform(X)
+
+#Init Linear SVM
+svm = LinearSVC(max_iter=10000)
+
+# Define the hyperparameter grid to search over
+param_grid = {
+    'C': [0.1, 1, 10],
+    'penalty': ['l1', 'l2'],
+}
+
+# Create the GridSearchCV object
+grid_search = GridSearchCV(svm, param_grid, cv=5)
+
+# Fit the GridSearchCV object to the data
+grid_search.fit(X_pca, y)
+
+# Print the best score and best parameters found by the grid search
+print(f"Best score: {grid_search.best_score_:.3f}")
+print(f"Best parameters: {grid_search.best_params_}")
+
+# Save the best model
+svm = LinearSVC(penalty=grid_search.best_params_['penalty'], C=grid_search.best_params_['C'], max_iter=10000)
+# Wrap the model in CalibratedClassifierCV to enable probability estimates
+svm = CalibratedClassifierCV(svm, cv=5, method='isotonic')
+svm.fit(X_pca, y)
+
+# Save the trained model and PCA object to a file
+with open('SVM.pkl', 'wb') as f:
+    pickle.dump(svm, f)
+
+with open('pca.pkl', 'wb') as f:
+    pickle.dump(pca, f)
